20c45aaff4b4e4ce04b263a2375de273a59e4711
lgarcia <leonardo.garcia@coasinlogicalis.com>
2019-10-07 15:35:47 -0300

    Primer commit

---
+++ _js/config.js
@@ -0,0 +1,10 @@
+const _MSG_LECTURA_REQUEST_ = ['Transaccion', 'Identificador']
+
+const _MSG_LECTURA_RESPONSE_ = ['Transaccion', 'Identificador']
+
+
+const _MSG_LIST_ = {
+    LECTURA_REQUEST : _MSG_LECTURA_REQUEST_,
+    LECTURA_RESPONSE : _MSG_LECTURA_RESPONSE_
+}
+    
