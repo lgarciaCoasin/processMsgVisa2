08e9797548aafe75ee8546fb96ebca29661176ed
lgarcia <leonardo.garcia@coasinlogicalis.com>
2019-10-09 13:34:51 -0300

    Modificaciones varias: * Se cambia nombre a la lista de los tipos de mensajes. * Se añade utilitario.js al index.html

--- _js/config.js
+++ _js/config.js
@@ -3,7 +3,7 @@ const _MSG_LECTURA_REQUEST_ = ['Transaccion', 'Identificador']
 const _MSG_LECTURA_RESPONSE_ = ['Transaccion', 'Identificador']
 
 
-const _MSG_LIST_ = {
+const _MSG_LIST_NAME_FIELD = {
     LECTURA_REQUEST : _MSG_LECTURA_REQUEST_,
     LECTURA_RESPONSE : _MSG_LECTURA_RESPONSE_
 }
