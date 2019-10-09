ab2cf9fcd616590bcd9e20fca3aa1532540adc2b
lgarcia <leonardo.garcia@coasinlogicalis.com>
2019-10-09 14:46:52 -0300

    Refactorizacion a la clase BusineddIndex: * Se agrega metodos que procesan el valor y nombre de los campos del mensaje visa2. * Se agrega metodos que maneja la logica de procesar y borrar el msg visa2. * Se agrega metodo que maneja la seleccion del tipo de mensaje.

--- _js/businessIndex.js
+++ _js/businessIndex.js
@@ -1,26 +1,57 @@
 class BusinessIndex {
     constructor (){
         this.modelIndex = new ModelIndex()
+        this.visa2 = new Visa2()
     }
 
-    clear(){
-        this.modelIndex.addListener(this.modelIndex.btoClear, 'click', ()=>{
-            this.modelIndex.setValueElement(this.modelIndex.msgVisa2, '')
-            this.modelIndex.setValueElement(this.modelIndex.nameField, '')
-            this.modelIndex.eraseAllRowTable(this.modelIndex.tableData)
+    loadTypeMsgToSelect(){
+        for(let propetyObject in _MSG_LIST_NAME_FIELD){
+            this.modelIndex.addOptionSelectTypeMsg(`${propetyObject}`, `${propetyObject}`)
+        }
+    }
+
+    managerSelectTypeMsg(){
+        this.modelIndex.anagerEventChangeSelectTypeMsg(() => {
+            this.modelIndex.tagInputNameField.value = ''
+            this.modelIndex.tagInputNameField.disableb = (this.modelIndex.tagSelectTypeMsg === 'otro') ? false : true;
         })
     }
 
-    processMsg(){
-        this.modelIndex.addListener(this.modelIndex.btoProcess, 'click', ()=>{
-            let valueCells ={
-                cell1 : '1',
-                cell2 : '2',
-                cell3 : '3',
-                cell4 : '4',
-            }
-           
-            this.modelIndex.addRowTable(this.modelIndex.tableData, valueCells)
-        }) 
+    managerProcessReleaseData(){
+        this.modelIndex.managerEventClickBtnClear(() => {
+            this.modelIndex.tagTextAreaMsgVisa2.value = ''
+            this.modelIndex.eraseAllRegistryTable()
+        })
     }
+
+    processingMsgVisa2NameField(){
+        const listNameFieldMsgVisa2 = this.modelIndex.tagInputNameField.value
+        if(this.modelIndex.tagSelectTypeMsg === 'otro')
+            this.visa2.msgNameField = listNameFieldMsgVisa2.split(',')
+        else
+            this.visa2.msgNameField = _MSG_LIST_NAME_FIELD[this.modelIndex.tagSelectTypeMsg.value]
+    }
+
+    processingMsgVisa2ValueField(){
+        this.visa2.msgToProcess = this.modelIndex.tagTextAreaMsgVisa2.value
+        this.visa2.processMsgVisa2('|')
+    }
+
+    showResultProcesingMsgVisa2(){
+        this.modelIndex.managerEventClickBtnProcess(() => {
+            this.processingMsgVisa2ValueField()
+            this.processingMsgVisa2NameField()
+
+            this.visa2.msgValueField.forEach((element, index) => {
+                let registerMsgVisa2 = {
+                    countField : index + 1,
+                    nameField : this.visa2.msgNameField[index],
+                    LengthField : element.length,
+                    valueField : element
+                }
+                this.modelIndex.addOneRegistryTable(registerMsgVisa2)
+            });
+        })
+    }
+
 }
