af4b8fa78effa51282ef4f80407af17ec31b4d65
lgarcia <leonardo.garcia@coasinlogicalis.com>
2019-10-09 10:27:34 -0300

    Nuevas funcionalidades en clase utilitaria: * Metodo para agregar opciones a tag html select * Metodo para agregar filas a cualquier tabla en cualquier tag, como thead, tbody y tfooter * Metodo para agregar celda a una fila * Metodo que borra todas las filas de una tabla en cualquier tag, como thead, tbody y tfooter * Metodo que permirte manejar la propiedad disbaled de cualquier elemento Html

--- _js/utilitarian.js
+++ _js/utilitarian.js
@@ -3,4 +3,31 @@ class Utilitarian {
     addEventListenerTagHtml(elementHtml, typeEvent, functionCallBack){
         elementHtml.addEventListener(typeEvent, functionCallBack)
     }
+
+    addOptionTagSelect(elementSelect, valueOption, textOption){
+        const option = document.createElement('option')
+        option.value = valueOption
+        option.text = textOption
+        elementSelect.add(option, null)
+    }
+
+    addRowTable(elementTable, tagTable){
+        const tagInsertRowTable = elementTable.getElementsByTagName(tagTable)[0]
+        const countRowExisting = tagInsertRowTable.getElementsByTagName('tr').length
+        return tagInsertRowTable.insertCell(countRowExisting)
+    }
+
+    addCellTable(elementRowTable, dataForCell){
+        const countCellExisting = elementRowTable.getElementsByTagName('td').length
+        const newCell = elementRowTable.insertCell(countCellExisting)
+        newCell.innerHTML = dataForCell
+    }
+
+    eraseAllRowTable(elementTable, tagTable){
+        elementTable.getElementsByTagName(tagTable)[0].innerHTML = ''
+    }
+
+    managerPropertyDisableElement(elementHtml, booleanDisabled){
+        elementHtml.disabled = booleanDisabled
+    }
 }
